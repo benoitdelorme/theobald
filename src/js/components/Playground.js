1694546531fb79df5ecba72f77c5dd3f1ca81b9f
Benoit Delorme <ben@leeroy.com>
2022-01-31 08:41:10 -0500

    spotlight

--- src/js/components/Playground.js
+++ src/js/components/Playground.js
@@ -1,8 +1,6 @@
 import * as io from 'socket.io-client';
 import gsap from 'gsap';
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 
 class Playground {
     constructor(el) {
@@ -10,22 +8,79 @@ class Playground {
         this.initCache();
         this.init();
         this.initEvents();
-        this.initWebgl();
+
+        this.render();
     }
 
     initCache() {
-        this.url = window.location.origin;
-        this.socket = io.connect(this.url);
-        this.userContainer = this.root.querySelector(".users");
+        //Client Server
+        this.url = window.location.origin
+        this.socket = io.connect(this.url)
+        this.clients = {}
+        this.users = {}
+        this.spotLightUsers = {}
+        /* this.spotLightMouseCoordinateX = {}
+        this.spotLightMouseCoordinateY = {} */
+
+        this.spotLightMouseCoordinate = []
+
+        //2D Scene
+        this.userContainer = this.root.querySelector(".users")
         this.descriptionSpans = this.userContainer.querySelectorAll('span')
-        this.currentUser = document.querySelectorAll(".user")[0];
-        this.currentUserLeeroy = this.currentUser.querySelector('.leeroy');
-        this.clients = {};
-        this.users = {};
-        /* this.currentUser.style.borderRadius = `${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% / ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}%`; */
+        this.currentUser = document.querySelectorAll(".user")[0]
+        this.currentUserLeeroy = this.currentUser.querySelector('.leeroy')
+        
+        //3D Scene
+        this.loader = new THREE.TextureLoader()
+        this.height = this.loader.load('./images/cloud.jpg')
+        this.texture = this.loader.load('./images/texture.jpeg')
+        this.alpha = this.loader.load('./images/alpha.png')
+        this.canvas = this.root.querySelector('canvas.webgl')
+
+        this.scene = new THREE.Scene()
+        this.geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
+        this.material = new THREE.MeshStandardMaterial({
+            color: 'grey',
+            map: this.texture,
+            displacementMap: this.height,
+            alphaMap: this.alpha,
+            transparent: true,
+            depthTest: false
+        })
+
+        this.plane = new THREE.Mesh(this.geometry, this.material)
+        this.pointLight = new THREE.PointLight("#00b3ff", 2)
+        this.spotLight = new THREE.SpotLight( 0xff9000, 9, 1.5, Math.PI * 0.04, 0.25, 0 );
+
+        this.sizes = {
+            width: window.innerWidth/2,
+            height: window.innerHeight-100
+        }
+
+        this.camera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvas
+        })
+        
+        this.clock = new THREE.Clock()
+
+        this.mouseY = 0;
+        this.mouseX = 0;
+        this.percentY = 0;
+        this.percentX = 0;
     }
 
     initEvents() {
+        window.addEventListener('resize', () => {
+            this.sizes.width = window.innerWidth/2
+            this.sizes.height = window.innerHeight-100
+
+            this.camera.aspect = this.sizes.width / this.sizes.height
+            this.camera.updateProjectionMatrix()
+
+            this.renderer.setSize(this.sizes.width, this.sizes.height)
+            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        })
 
         this.root.addEventListener('mousemove', (e) => {
             this.socket.emit("mousemove", {
@@ -38,14 +93,24 @@ class Playground {
             let calcX = e.pageX - 55;
             let calcY = e.pageY - 55;
 
+            this.percentX = (e.pageX/this.sizes.width - 0.5) * 2.5
+            this.percentY = (e.pageY/this.sizes.height - 0.5) * 2.5
+
             gsap.to(this.currentUser, 0.6, {x: e.pageX, y: e.pageY});
             gsap.to(this.currentUserLeeroy, 0.6, {x: -calcX, y: -calcY});
         });
         
+        //Client Server
         this.socket.on("moving", (data) => {
             if (!this.clients.hasOwnProperty(data.id)) {
                 this.addClient(data);
             }
+            
+            this.spotLightMouseCoordinate.push({
+                id: data.id,
+                x: (data.x/this.sizes.width - 0.5) * 2.5, 
+                y: (data.y/this.sizes.height - 0.5) * 2.5
+            })
             
             gsap.to(this.users[data.id], 0.6, {x: data.x, y: data.y});
             gsap.to(this.users[data.id].querySelector('.leeroy'), 0.6, {x: -data.calcX, y: -data.calcY});
@@ -58,7 +123,66 @@ class Playground {
             if (this.users[id]) {
                 this.users[id].parentNode.removeChild(this.users[id]);
             }
+
+            if (this.spotLightUsers[id]) {
+                this.scene.remove(this.spotLightUsers[id])
+
+                for(let o = 0; o < this.spotLightMouseCoordinate.length; o++) {
+                    if(this.spotLightMouseCoordinate[o].id == id) {
+                        
+                    }
+                }
+            }
+
         });
+
+    }
+    
+    addClient(data) {
+        //2D Scene
+        this.users[data.id] = this.userContainer.appendChild(this.currentUser.cloneNode(true));
+        this.users[data.id].querySelectorAll('path').forEach((user) => {
+            user.style.fill = this.getRandomColor();
+        })
+
+        this.users[data.id].querySelectorAll('span').forEach((span) => {
+            span.style.color = this.getRandomColor();
+        })
+
+        //3D Scene
+        this.spotLightUsers[data.id] = new THREE.SpotLight(this.getRandomColor(), 9, 1.5, Math.PI * 0.04, 0.25, 0 );
+        this.scene.add(this.spotLightUsers[data.id])
+        this.scene.add(this.spotLightUsers[data.id].target)
+    }
+
+    init() {
+        //2D Scene
+        this.currentUserLeeroy.querySelectorAll('path').forEach((user) => {
+            user.style.fill = this.getRandomColor();
+        })
+
+        this.descriptionSpans.forEach((span) => {
+            span.style.color = this.getRandomColor();
+        })
+
+        //3D Scene
+        this.plane.rotation.x = 181
+        this.pointLight.position.x = 0.2
+        this.pointLight.position.y = 10
+        this.pointLight.position.z = 4.4
+        this.spotLight.position.set( 0, 1.5, 0 );
+        this.camera.position.x = 0
+        this.camera.position.y = 1
+        this.camera.position.z = 3
+        
+        this.scene.add(this.plane);
+        this.scene.add(this.pointLight)
+        this.scene.add(this.spotLight);
+        this.scene.add(this.spotLight.target);
+        this.scene.add(this.camera)
+        
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     }
 
     getRandomColor() {
@@ -71,128 +195,20 @@ class Playground {
 
         return color;
     }
-    
-    addClient(data) {
-        this.users[data.id] = this.userContainer.appendChild(this.currentUser.cloneNode(true));
-        /* this.users[data.id].style.borderRadius = `${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% / ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}% ${Math.floor(Math.random() * 100)}%`; */
-        this.users[data.id].querySelectorAll('path').forEach((user) => {
-            user.style.fill = this.getRandomColor();
-        })
 
-        this.users[data.id].querySelectorAll('span').forEach((span) => {
-            span.style.color = this.getRandomColor();
-        })
-    }
+    render() {
+        const tick = () => {
+            const elapsedTime = this.clock.getElapsedTime()
+            this.plane.rotation.z = 0.3 * elapsedTime;
+            this.spotLight.target.position.z = this.percentY;
+            this.spotLight.target.position.x = this.percentX;
 
-    init() {
-        this.currentUserLeeroy.querySelectorAll('path').forEach((user) => {
-            user.style.fill = this.getRandomColor();
-        })
+            this.spotLightMouseCoordinate.forEach((spot) => {
+                this.spotLightUsers[spot.id].target.position.z = spot.y
+                this.spotLightUsers[spot.id].target.position.x = spot.x
+            })
 
-        this.descriptionSpans.forEach((span) => {
-            span.style.color = this.getRandomColor();
-        })
-    }
-
-    initWebgl() {
-        const loader = new THREE.TextureLoader()
-        const height = loader.load('./images/cloud.jpg')
-        const texture = loader.load('./images/texture.jpeg')
-        const alpha = loader.load('./images/alpha.png')
-
-        const canvas = document.querySelector('canvas.webgl')
-        const scene = new THREE.Scene()
-        const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
-
-        const material = new THREE.MeshStandardMaterial({
-            color: 'grey',
-            map: texture,
-            displacementMap: height,
-            alphaMap: alpha,
-            transparent: true,
-            depthTest: false
-        })
-
-        const plane = new THREE.Mesh(geometry, material)
-        scene.add(plane);
-
-        plane.rotation.x = 181
-
-        const pointLight = new THREE.PointLight("#00b3ff", 2)
-        pointLight.position.x = 0.2
-        pointLight.position.y = 10
-        pointLight.position.z = 4.4
-
-        scene.add(pointLight)
-
-        const spotLight = new THREE.SpotLight( 0xff9000, 9, 1.5, Math.PI * 0.04, 0.25, 0 );
-        spotLight.position.set( 0, 1.5, 0 );
-
-        scene.add(spotLight);
-        scene.add(spotLight.target);
-
-        const spotLightHelper = new THREE.SpotLightHelper( spotLight );
-        /* scene.add( spotLightHelper ); */
-
-        const sizes = {
-            width: window.innerWidth/2-50,
-            height: window.innerHeight+100
-        }
-
-        window.addEventListener('resize', () =>
-        {
-            sizes.width = window.innerWidth/2-50
-            sizes.height = window.innerHeight+100
-
-            camera.aspect = sizes.width / sizes.height
-            camera.updateProjectionMatrix()
-
-            renderer.setSize(sizes.width, sizes.height)
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        })
-
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-        camera.position.x = 0
-        camera.position.y = 1
-        camera.position.z = 3
-        scene.add(camera)
-
-        const controls = new OrbitControls(camera, canvas)
-        controls.enableDamping = true
-
-        const renderer = new THREE.WebGLRenderer({
-            canvas: canvas
-        })
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-
-        const clock = new THREE.Clock()
-        document.addEventListener('mousemove', animateMouse)
-
-        let mouseY = 0;
-        let mouseX = 0;
-        let percentY = 0;
-        let percentX = 0;
-
-        function animateMouse(e) {
-            mouseY = e.clientY;
-            mouseX = e.clientX;
-            
-            percentY = (mouseY/sizes.height - 0.5) * 2.5
-            percentX = (mouseX/sizes.width - 0.5) * 2.5
-        }
-
-        const tick = () =>
-        {
-
-            const elapsedTime = clock.getElapsedTime()
-            plane.rotation.z = 0.3 * elapsedTime;
-
-            spotLight.target.position.z = percentY;
-            spotLight.target.position.x = percentX;
-
-            renderer.render(scene, camera)
+            this.renderer.render(this.scene, this.camera)
             window.requestAnimationFrame(tick)
         }
 
